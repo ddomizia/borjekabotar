31944509a916b3e63c7809c0880dc2c92301e41d
ddomizia <domizia.derasmo@gmail.com>
2022-02-14 20:31:09 +0100

    Eliminato bold nel meta Helmet

--- src/templates/Layout.js
+++ src/templates/Layout.js
@@ -16,9 +16,7 @@ const Layout = ({ children }) => {
         <link rel="shortcut icon" type="image/x-icon" href={Favicon_32} />
         <link rel="shortcut icon" type="image/x-icon" href={Favicon_ico} />
 
-        <title>
-          Borj-e Kabotar | Home
-        </title>
+        <title>Borj-e Kabotar | Home</title>
 
         <meta name="description" content="Borj-e Kabotar" />
         <link rel="canonical" href="https://borjekabotar.com" />
@@ -26,7 +24,7 @@ const Layout = ({ children }) => {
         <meta property="og:title" content="Borj-e Kabotar" />
         <meta
           property="og:description"
-          content="The towers for pigeons are widespread in the areas around **Isfahan** and **Golpayegan** and even today these buildings characterise the landscape. They are often imposing structures that can sometimes be mistaken for a defensive system. The earliest **towers** in the province of Isfahan date back to the 16th-17th centuries."
+          content="The towers for pigeons are widespread in the areas around Isfahan and Golpayegan and even today these buildings characterise the landscape. They are often imposing structures that can sometimes be mistaken for a defensive system. The earliest **towers** in the province of Isfahan date back to the 16th-17th centuries."
         />
         <meta property="og:url" content="https://borjekabotar.com" />
         <meta
@@ -36,11 +34,11 @@ const Layout = ({ children }) => {
 
         <meta
           property="twitter:title"
-          content="The towers for pigeons are widespread in the areas around **Isfahan** and **Golpayegan** and even today these buildings characterise the landscape. They are often imposing structures that can sometimes be mistaken for a defensive system. The earliest **towers** in the province of Isfahan date back to the 16th-17th centuries."
+          content="The towers for pigeons are widespread in the areas around Isfahan and Golpayegan and even today these buildings characterise the landscape. They are often imposing structures that can sometimes be mistaken for a defensive system. The earliest **towers** in the province of Isfahan date back to the 16th-17th centuries."
         />
         <meta
           property="twitter:description"
-          content="The towers for pigeons are widespread in the areas around **Isfahan** and **Golpayegan** and even today these buildings characterise the landscape. They are often imposing structures that can sometimes be mistaken for a defensive system. The earliest **towers** in the province of Isfahan date back to the 16th-17th centuries."
+          content="The towers for pigeons are widespread in the areas around Isfahan and Golpayegan and even today these buildings characterise the landscape. They are often imposing structures that can sometimes be mistaken for a defensive system. The earliest **towers** in the province of Isfahan date back to the 16th-17th centuries."
         />
         <meta property="twitter:url" content="https://lad.saras.uniroma1.it" />
         <meta
